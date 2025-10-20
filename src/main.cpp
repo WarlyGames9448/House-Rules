cfe62fceade62ff8217036f56cb3c2b451d87487
WarlyGames9448 <felixwesley155@gmail.com>
2025-10-19 21:21:51 -0300

    plataform correction

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 
+#include <GLFW/glfw3native.h>
+
 #include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
 
 int main() {
-    // Inicializa GLFW
-    if (!glfwInit()) {
-        std::cerr << "Falha ao inicializar GLFW!" << std::endl;
-        return -1;
+
+    // Initialize GLFW
+    glfwInit();
+
+    if (!glfwPlatformSupported(GLFW_PLATFORM_WAYLAND)) {
+        glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
     }
 
-    // Define a versão do OpenGL (3.3 Core)
+    // Defines OpenGL version (3.3 Core)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Cria a janela
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Olá OpenGL!", NULL, NULL);
+    // Create Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Um game", NULL, NULL);
+
     if (window == NULL) {
-        std::cerr << "Falha ao criar a janela GLFW!" << std::endl;
+        std::cerr << "Error to create window." << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Falha ao inicializar GLAD!" << std::endl;
-        return -1;
-    }
-
-
-    // Loop principal da aplicação
+    // Principal Loop
     while (!glfwWindowShouldClose(window)) {
         // Limpa a tela com uma cor
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
 
         // Processa eventos e troca os buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Finaliza GLFW
+    //Finish GLFW
     glfwTerminate();
     return 0;
 }
