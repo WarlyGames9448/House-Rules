cc487e3ecf568151b9d5c959cbe2292ba7d92c97
WarlyGames9448 <felixwesley155@gmail.com>
2026-03-05 21:20:51 -0300

    Buffer Layout Abstraction

--- Fuze/include/Plataform/OpenGL/OpenGLBuffer.h
+++ Fuze/include/Plataform/OpenGL/OpenGLBuffer.h
@@ -9,11 +9,15 @@ namespace Fuze {
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         ~OpenGLVertexBuffer();
 
-        void Bind() const;
-        void Unbind() const;
+        void Bind() const override;
+        void Unbind() const override;
+
+        virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+        virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 
       private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer {
--- Fuze/src/Application.cpp
+++ Fuze/src/Application.cpp
@@ -9,6 +9,37 @@ namespace Fuze {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
+    static GLenum ShaderDataTypeToOpenGLDataType(ShaderDataType type) {
+        switch (type) {
+        case ShaderDataType::Float:
+            return GL_FLOAT;
+        case ShaderDataType::Float2:
+            return GL_FLOAT;
+        case ShaderDataType::Float3:
+            return GL_FLOAT;
+        case ShaderDataType::Float4:
+            return GL_FLOAT;
+        case ShaderDataType::Mat3:
+            return GL_FLOAT;
+        case ShaderDataType::Mat4:
+            return GL_FLOAT;
+        case ShaderDataType::Int:
+            return GL_INT;
+        case ShaderDataType::Int2:
+            return GL_INT;
+        case ShaderDataType::Int3:
+            return GL_INT;
+        case ShaderDataType::Int4:
+            return GL_INT;
+        case ShaderDataType::Bool:
+            return GL_BOOL;
+        case ShaderDataType::None:
+            return GL_NONE;
+        }
+
+        return 0;
+    };
+
     Application* Application::s_Instance = nullptr;
 
     Application::Application() {
@@ -22,10 +53,10 @@ namespace Fuze {
         m_ImGuiLayer = new ImGuiLayer();
         PushOverlay(m_ImGuiLayer);
 
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f, //
-            0.5f,  -0.5f, 0.0f, //
-            0.5f,  0.5f,  0.0f, //
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //
+            0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //
+            0.0f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //
         };
 
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
@@ -34,30 +65,47 @@ namespace Fuze {
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        BufferLayout layout = {{ShaderDataType::Float3, "a_Position"}, {ShaderDataType::Float4, "a_Color"}};
+
+        m_VertexBuffer->SetLayout(layout);
+
+        uint32_t index = 0;
+        for (const auto& element : m_VertexBuffer->GetLayout()) {
+
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLDataType(element.Type),
+                                  element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(),
+                                  (const void*)(uintptr_t)element.Offset);
+            index++;
+        }
 
         uint32_t indices[3] = {0, 1, 2};
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices)));
 
         const char* vertexShaderSource = R"(
             #version 330 core
-            layout (location = 0) in vec3 aPos;
+            layout (location = 0) in vec3 a_Position;
+            layout (location = 1) in vec4 a_Color;
 
-            out vec3 vPos;
+            out vec3 v_Position;
+            out vec4 v_Color;
             void main() {
-                vPos = aPos;
-                gl_Position = vec4(aPos, 1.0);
+                v_Position = a_Position;
+                v_Color = a_Color;
+                gl_Position = vec4(a_Position, 1.0);
             }
         )";
 
         const char* fragmentShaderSource = R"(
             #version 330 core
-            layout (location = 0) out vec4 FragColor;
-            in vec3 vPos;
+            layout (location = 0) out vec4 color;
+
+            in vec3 v_Position;
+            in vec4 v_Color;
 
             void main() {
-                FragColor = vec4(vPos, 1.0);
+                color = vec4(v_Position * 0.5 + 0.5, 1.0f);
+                color = v_Color;
             }
         )";
 
